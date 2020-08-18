c51fd550f482390ddbb9401373dc6f9fec45b324
Martin Strandbygaard <martin@strandbygaard.net>
2020-08-18 12:21:38 +0200

    Use config package to read configurations

--- R/sample.R
+++ R/sample.R
@@ -1,3 +1,10 @@
+if (!require("config")) install.packages("config")
+
+Sys.setenv(R_CONFIG_FILE = "config/config.yml")
+Sys.setenv(R_CONFIG_ACTIVE = "default")
+
+param = config::get("dataset")
+
 sample_function <- function(x) {
   return(2 * x);
 }
